57c8b1227cd9be49d4a4deb253faf3992cad0010
Alexandru Lolea <alexandru.n.lolea@gmail.com>
2026-01-20 12:07:52 +0100

    ✨ Completed forwaard propagation

--- main.py
+++ main.py
@@ -16,6 +16,21 @@ def init_params():
     
     return W1, b1, W2, b2
 
+def ReLU(Z):
+    return np.maximum(0, Z)
+
+def softmax(Z):
+    return np.exp(Z) / np.sum(np.exp(Z))
+
+def forward_propagation(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+
+    return Z1, A1, Z2, A2
+
 data = pd.read_csv('mnist_data/train.csv')
 
 # Convert panda dataframe to numpy array
